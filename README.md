454f798c4f4568391e09d02afae9987b4889cf69
Thomas <875831461@qq.com>
2021-08-18 11:46:30 +0800

    this is just push how to use thomas printer library

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 ThomasPrinter
+====
+use for bluetooth Thermal Printer
+-------
+![](http://photocq.photo.store.qq.com/psc?/V12Wa3Ul4PEo0g/jkqgNxaPJb7RsklupiKoXbvsqI13FZVRI5eBTK1Z6.LFL6E.2Co2ikxdWdI.q9ZxRIyQMUC55I1FIrPwTX9uwD73TqaU4xx2.gUjAYKsYeI!/b&bo=ZABkAGQAZAADGD0!&rf=viewer_4)
--- app/src/main/java/thomas/printer/MainActivity.java
+++ app/src/main/java/thomas/printer/MainActivity.java
@@ -1,5 +1,6 @@
 package thomas.printer;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
@@ -9,23 +10,57 @@ import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
+import com.thomas.ThermalPrinter;
 import com.thomas.ThomasPrinterManage;
+import com.thomas.factory.Alignment;
+import com.thomas.factory.FontSize;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Vector;
 
 import thomas.printer.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
-    private ThomasPrinterManage.OnThomasPrinterChangeListener mPrinterChangeListener = new ThomasPrinterManage.OnThomasPrinterChangeListener() {
-        @Override
-        public void onThomasPrinterChanged(@Nullable BluetoothSocket bluetoothSocket, int i) {
-            Toast.makeText(MainActivity.this, "" + i, Toast.LENGTH_SHORT).show();
 
+    /**
+     * register this listener will known printer connect or send state
+     */
+    private ThomasPrinterManage.OnThomasPrinterChangeListener mPrinterChangeListener = new ThomasPrinterManage.OnThomasPrinterChangeListener() {
+        /**
+         * when you connect or send data it will tell you
+         * @param bluetoothSocket bluetoothSocket
+         * @param state printer state
+         */
+        @Override
+        public void onThomasPrinterChanged(@Nullable BluetoothSocket bluetoothSocket, int state) {
+            switch (state){
+                case ThomasPrinterManage.STATE_DISCONNECTED:
+                    System.out.println("");
+                    break;
+                case ThomasPrinterManage.STATE_CONNECTING:
+                    break;
+                case ThomasPrinterManage.STATE_CONNECTED:
+                    break;
+                case ThomasPrinterManage.STATE_CONNECTED_FAILED:
+                    break;
+                case ThomasPrinterManage.STATE_ADDRESS_ERROR:
+                    break;
+                case ThomasPrinterManage.STATE_NO_DEVICE:
+                    break;
+                case ThomasPrinterManage.STATE_SEND_SUCCESS:
+                    break;
+                case ThomasPrinterManage.STATE_SEND_FAILED:
+                    break;
+            }
         }
     };
 
@@ -37,6 +72,9 @@ public class MainActivity extends AppCompatActivity {
         initPermission();
     }
 
+    /**
+     * After Android API 26, you will not be able to search for Bluetooth devices without this location permission
+     */
     private void initPermission() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             if((checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) ){
@@ -48,6 +86,11 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        // do something here when permission is refuse or ....
+    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
@@ -66,12 +109,48 @@ public class MainActivity extends AppCompatActivity {
 
     public void onConnectClick(View view) {
         String address = (String) binding.buttonConnect.getTag();
-        System.out.println(address);
         ThomasPrinterManage.getInstance().connectBluetoothDevice(address);
     }
 
     public void onSendClick(View view) {
-
+        Vector<Byte> data = new Vector<>();
+        ThermalPrinter thermalPrinter = new ThermalPrinter();
+        thermalPrinter.setCommand(data);
+        thermalPrinter.initPrint();
+        // according to your paper size
+        thermalPrinter.setPageSizeEighty();
+        //thermalPrinter.setPageSizeFiftyEight();
+        thermalPrinter.setWhiteOnBlack();
+        thermalPrinter.setAlignment(Alignment.CENTER);
+        thermalPrinter.setFontSize(FontSize.LARGER);
+        thermalPrinter.setText(getTextByte("White On Black"));
+        thermalPrinter.cancelWhiteOnBlack();
+        thermalPrinter.printAndFeedLine();
+        thermalPrinter.putText(getTextByte("content"), FontSize.NORMAL, Alignment.CENTER,false);
+        thermalPrinter.printAndFeedLine();
+        thermalPrinter.putText(getTextByte("content"), FontSize.NORMAL, Alignment.RIGHT,true);
+        thermalPrinter.printAndFeedLine();
+        thermalPrinter.putText(getTextByte("content"), FontSize.BIG, Alignment.RIGHT,false);
+        thermalPrinter.printAndFeedLine();
+        thermalPrinter.putText(getTextByte("content"), FontSize.LARGER, Alignment.RIGHT,false);
+        thermalPrinter.printAndFeedLine();
+        thermalPrinter.setUnderline(true);
+        thermalPrinter.setText(getTextByte("content"));
+        thermalPrinter.setUnderline(false);
+        thermalPrinter.printAndFeedLine();
+        thermalPrinter.putTextColumn(getTextByte("column one"), getTextByte("column two"),FontSize.NORMAL);
+        thermalPrinter.putTextColumn(getTextByte("column one"), getTextByte("column two"),FontSize.NORMAL);
+        thermalPrinter.putTextColumn(getTextByte("column one"), getTextByte("column two"),FontSize.BIG);
+        thermalPrinter.putTextColumn(getTextByte("column one"), getTextByte("column two"),FontSize.NORMAL);
+        // not useful for XML images
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.smart);
+        thermalPrinter.putBitmap(bitmap);
+        thermalPrinter.printBarCode("12345684",Alignment.CENTER,3,80);
+        // this need printer support
+        thermalPrinter.printQrCode(Alignment.CENTER,3,48,"thomas printer");
+        thermalPrinter.setCutPaper();
+        // if you connect more than one devices you can use this method
+        ThomasPrinterManage.getInstance().writeSendDataAllDevice(data);
     }
 
     public void onCancelObserveClick(View view) {
@@ -81,6 +160,19 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        ThomasPrinterManage.getInstance().stopThreadPool();
+        ThomasPrinterManage.getInstance().unregisterOnThomasPrinterChangeListener(mPrinterChangeListener);
+        // When you exit the application, close the accept print task
+        // But the tasks before the shutdown will be executed
+        ThomasPrinterManage.getInstance().stopAcceptPrintTask();
+    }
+
+    private byte[] getTextByte(@NonNull String content) {
+        byte[] bytes = new byte[0];
+        try {
+            bytes = content.getBytes("GB18030");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return bytes;
     }
 }
